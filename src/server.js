25bd5ebc81d5848af5cd4d8915a0b59d6aa32f6b
sally <sally@geekstudio.co.kr>
2023-02-09 12:08:22 +0900

    Get requests and respondses

--- src/server.js
+++ src/server.js
@@ -4,6 +4,19 @@ import express, { application } from "express";
 const PORT = 4000;
 const app = express();
 
+/* 누군가가 어떤 Route로(home으로 Get request를 보낸다면) */
+const handleHome = (req, res) => {
+    return res.send("hi");
+}
+
+const handleLogin = (req, res) => {
+    return res.send("<h1>Login Here</h1>");
+}
+
+app.get("/", handleHome);
+//handleHome({Request Object...}, {Response Object...})
+app.get("/login", handleLogin)
+
 const handleListening = () =>
     console.log(`Server listening on http://localhost:${PORT} 🚀`);
 
